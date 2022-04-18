ef369570e8b27d45a3e12d2e5f7af0f6ecaef626
mkalansari7 <mk.alansari7@gmail.com>
2022-04-18 07:34:29 +0300

    semester model / CRUD work

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+import semesters
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'corsheaders',
+    'semesters',
 ]
 
 MIDDLEWARE = [
