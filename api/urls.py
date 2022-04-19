2acf5a6c5d7f820f72476404dc47f1a247adb1d5
mkalansari7 <mk.alansari7@gmail.com>
2022-04-19 22:18:03 +0300

    add project router

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path, include
 from semesters.views import SemesterListSet
+from projects.views import ProjectListSet
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register("semester", SemesterListSet)
+router.register("project", ProjectListSet)
+
 
 urlpatterns = [
     # ? auth 
     path('', include('djoser.urls')),
     path('', include('djoser.urls.jwt')),
 
-    # ? Semester
+    # ? Semester & Project
     path("", include(router.urls)),
+
+    
     
 ]
