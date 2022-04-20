9c12dbe091d36dc7643e9ed3cf4f712fed75415c
mkalansari7 <mk.alansari7@gmail.com>
2022-04-20 06:57:43 +0300

    CRUD for teams

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from semesters.views import SemesterListSet
 from projects.views import ProjectListSet
+from teams.views import TeamListSet
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register("semester", SemesterListSet)
 router.register("project", ProjectListSet)
+router.register("team", TeamListSet)
 
 
 urlpatterns = [
