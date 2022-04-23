242bb84190dd79f07da1e82b3ae98576fae884c0
mkalansari7 <mk.alansari7@gmail.com>
2022-04-23 21:33:51 +0300

    Criteria model / CRUD

--- criterias/views.py
+++ criterias/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Criteria
+from .serializers import CriteriaListSerializer
 
 # Create your views here.
+
+class CriteriaListSet(ModelViewSet):
+    queryset = Criteria.objects.all()
+    serializer_class = CriteriaListSerializer
--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,11 @@
 from re import T
 from django.db import models
 from semesters.models import Semester
+from criterias.models import Criteria
 
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=255)
     wieght = models.IntegerField()
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name='project')
+    criteria = models.ManyToManyField(Criteria)
